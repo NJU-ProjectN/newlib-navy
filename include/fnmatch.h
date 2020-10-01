e5ac8fde72ee0e3dae40746113be68f0663333f8
Zihao Yu <yuzihao@ict.ac.cn>
2020-10-01 20:03:42 +0800

    include,fnmatch: fix compile error

--- include/fnmatch.h
+++ include/fnmatch.h
@@ -41,12 +41,10 @@
 #define	FNM_PATHNAME	0x02	/* Slash must be matched by slash. */
 #define	FNM_PERIOD	0x04	/* Period must be matched by period. */
 
-#if __GNU_VISIBLE
 #define	FNM_LEADING_DIR	0x08	/* Ignore /<tail> after Imatch. */
 #define	FNM_CASEFOLD	0x10	/* Case insensitive search. */
 #define	FNM_IGNORECASE	FNM_CASEFOLD
 #define	FNM_FILE_NAME	FNM_PATHNAME
-#endif
 
 __BEGIN_DECLS
 int	 fnmatch(const char *, const char *, int);
