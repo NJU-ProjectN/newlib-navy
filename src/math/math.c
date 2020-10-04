fd0eb9ecbdfd8dd5b4c9951dace673ead01f3669
Zihao Yu <yuzihao@ict.ac.cn>
2020-10-04 15:30:27 +0800

    add dummy math functions

---
+++ src/math/math.c
@@ -0,0 +1,32 @@
+#include <math.h>
+#include <assert.h>
+
+// Navy does not support floating point
+
+double fabs(double x) {
+  assert(0);
+}
+
+double sin(double x) {
+  assert(0);
+}
+
+double cos(double x) {
+  assert(0);
+}
+
+double pow(double x, double y) {
+  assert(0);
+}
+
+double log(double x) {
+  assert(0);
+}
+
+double round(double x) {
+  assert(0);
+}
+
+double floor(double x) {
+  assert(0);
+}
